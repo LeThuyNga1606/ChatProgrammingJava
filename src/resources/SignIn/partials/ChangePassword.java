f3833673bdf387cb9d784d4b41d888960d23b183
Tuan <pqt0142@gmail.com>
2022-12-23 18:00:58 +0700

    init sendmail recovery password base

---
+++ src/resources/SignIn/partials/ChangePassword.java
@@ -0,0 +1,13 @@
+package resources.SignIn.partials;
+
+import javax.swing.*;
+
+public class ChangePassword {
+    private JPanel mainPanel;
+    private JLabel titleLabel;
+    private JButton okButton;
+    private JPasswordField newPasswordField;
+    private JLabel newPasswordLabel;
+    private JTextField reEnterNewPasswordField;
+    private JLabel reEnterNewPasswordLabel;
+}
---
+++ src/resources/SignIn/partials/ForgotPassword.java
@@ -0,0 +1,19 @@
+package resources.SignIn.partials;
+
+import javax.swing.*;
+
+public class ForgotPassword {
+    private JPanel mainPanel;
+    private JLabel titleLabel;
+    private JTextField mailTextField;
+    private JLabel enterMailLabel;
+    private JButton okButton;
+
+    ForgotPassword() {
+
+    }
+
+
+
+
+}
