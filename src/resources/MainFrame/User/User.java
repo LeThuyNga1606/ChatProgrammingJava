55ed3a41bd571b419da620a12cc645a027856efa
LeThuyNga1606 <85446368+LeThuyNga1606@users.noreply.github.com>
2023-01-07 16:18:54 +0700

    update User.java

---
+++ src/resources/MainFrame/User/User.java
@@ -0,0 +1,57 @@
+package resources.MainFrame.User;
+import resources.SignIn.partials.ChangePassword;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class User extends JFrame{
+    private JPanel mainPanel;
+    private JButton ListFriend;
+    private JButton ListChat;
+    private JButton ListGroup;
+    private JButton ListUserOnline;
+    private JButton changePass;
+
+    public User(String username){
+        JFrame jr = new JFrame();
+        jr.setTitle("LOGIN - CHAT PROGRAMMING");
+        jr.add(mainPanel);
+        jr.setSize(400, 300);
+        jr.setLocationRelativeTo(null);
+        jr.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        jr.setVisible(true);
+        ListFriend.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ListChat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ListGroup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        ListUserOnline.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new ShowUserList1(username);
+            }
+        });
+        changePass.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new ChangePassword(username);
+            }
+        });
+    }
+}
